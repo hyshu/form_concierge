ec1af6052c9e67e36dcb7294f74593572ea3f05a
hyshu <mail.hayami@gmail.com>
2026-06-22 21:55:26 +0900

    Hydrate public form client from SSR payload

--- web/lib/main.client.dart
+++ web/lib/main.client.dart
@@ -1,11 +1,15 @@
 import 'package:jaspr/client.dart';
 
 import 'app.dart';
+import 'utils/ssr_payload.dart';
 
 const _serverUrl = 'http://localhost:8787';
 
 void main() {
+  final serverUrl = readConfiguredApiUrl() ?? _serverUrl;
+  removeSsrRoot();
+
   runApp(
-    const App(serverUrl: _serverUrl),
+    App(serverUrl: serverUrl),
   );
 }
---
+++ web/lib/utils/ssr_payload.dart
@@ -0,0 +1,2 @@
+export 'ssr_payload_stub.dart'
+    if (dart.library.js_interop) 'ssr_payload_web.dart';
